3f789391630c5f19112a8c43492043b4a44b2722
Alisson <rockspringnet@gmail.com>
2015-09-28 19:59:46 -0300

    Commit Inicial !

---
+++ android/src/br/com/FlappyBird/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package br.com.FlappyBird.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import br.com.FlappyBird.MyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGame(), config);
+	}
+}
